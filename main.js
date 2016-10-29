67e65c21022ade505677ec8d08e2e7ec853da98d
He Zhou <hezhou556@gmail.com>
2016-10-30 01:34:22 +0800

    Add countdown days

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
  * Created by Samuel on 29/10/2016.
  */
 
-var dcd = angular.module('countdownApp',['firebase']);
-dcd.controller('countdownController',function($scope ,$firebaseArray){
-   $scope.welcomeMsg = "Welcome to D!Countdown";
+var dcd = angular.module('countdownApp', ['firebase']);
+dcd.controller('countdownController', function ($scope, $firebaseArray) {
+    $scope.welcomeMsg = "Welcome to D!Countdown";
 
     var firebaseRef = firebase.database().ref().child("countdown");
     $scope.upcomings = $firebaseArray(firebaseRef);
@@ -14,12 +14,16 @@ dcd.controller('countdownController',function($scope ,$firebaseArray){
     $scope.eventDate = '';
     $scope.eventHour = '';
     $scope.eventMinute = '';
-    $scope.receiveForm = function(){
-        $scope.eventDate.setHours($scope.eventHour,$scope.eventMinute);
-        $scope.input = {"name":$scope.eventName,"description":$scope.eventDescription,"time":$scope.eventDate.getTime()};
+    $scope.receiveForm = function () {
+        $scope.eventDate.setHours($scope.eventHour, $scope.eventMinute);
+        $scope.input = {
+            "name": $scope.eventName,
+            "description": $scope.eventDescription,
+            "time": $scope.eventDate.getTime()
+        };
         $scope.eventName = '';
         $scope.eventDescription = '';
-        $scope.eventDate='';
+        $scope.eventDate = '';
         $scope.eventHour = '';
         $scope.eventMinute = '';
         console.log($scope.input);
@@ -28,14 +32,19 @@ dcd.controller('countdownController',function($scope ,$firebaseArray){
 
     //Manipulate HOW to display the time
     $scope.display = [];
-    for (var i=0; i<$scope.upcomings.length;i++){
-        $scope.display[i] = moment(new Date($scope.upcomings[i].time)).format("dddd, MMMM Do YYYY, h:mm:ss a");
-    }
+    $scope.detailedTime = [];
     $scope.$watch("upcomings", function (newValue, oldValue) {
         $scope.display = [];
-        for (var i = 0; i < newValue.length; i++){
+        for (var i = 0; i < newValue.length; i++) {
             var date = new Date(newValue[i].time);
-            $scope.display[i] = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+            //$scope.display[i] = moment().diff(moment(date),'days');
+            $scope.detailedTime[i] = moment(date).format('MMMM Do YYYY, h:mm:ss a');
+            if (moment(date).diff(moment(), 'days') === -1 ||moment(date).diff(moment(), 'days')===0||moment(date).diff(moment(), 'days')===1) {
+                $scope.display[i] = moment(date).diff(moment(), 'days') + ' Day';
+            }
+            else {
+                $scope.display[i] = moment(date).diff(moment(), 'days') + ' Days';
+            }
         }
     }, true);
 
